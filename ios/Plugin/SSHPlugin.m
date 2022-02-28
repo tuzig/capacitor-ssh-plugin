3ca117744e9d8a0160db85c55dc3f2a84cc1cdd4
Benny Daon <benny@tuzig.com>
2022-02-28 13:44:27 +0200

    Fixing the return types

--- ios/Plugin/SSHPlugin.m
+++ ios/Plugin/SSHPlugin.m
@@ -4,9 +4,10 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(SSHPlugin, "SSH",
-           CAP_PLUGIN_METHOD(startSessionByPasswd, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(startSessionByPasswd, CAPPluginReturnCallback);
            CAP_PLUGIN_METHOD(newChannel, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(closeChannel, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(startShell, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(closeChannel, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(closeSession, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(startShell, CAPPluginReturnCallback);
            CAP_PLUGIN_METHOD(writeToChannel, CAPPluginReturnPromise);
 )
